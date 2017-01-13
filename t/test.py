5cbfab143f225ab85e7e5036c9307e43c951e51f
U-OFFICEPC\John <johnmcg@gmail.com>
2017-01-12 22:20:01 -0800

    Added some to the test case

--- t/test.py
+++ t/test.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python
 
-exit();
+import sys
+sys.path.append('..')
+import unittest
+from token_data import TokenData
+
+class TestRosary(unittest.TestCase):
+   def testJoyful(self):
+       self._testMysteries('Joyful')
+
+   def _testMysteries(self,mysteries):
+       currentData = TokenData('Rosary',mysteries,'SignOfTheCross',0,0)
+       token = currentData.get_token();
+       newData = currentData.from_token(token)
+       self.assertEqual(currentData,newData)
+
+
+if __name__ == '__main__':
+    unittest.main()
